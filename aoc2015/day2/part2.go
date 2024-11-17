5dd9910349579b4237dab24ccb2db8f51e0d9b9c
Livingpool <52132459+Livingpool@users.noreply.github.com>
2024-11-17 10:08:00 +0800

    finish day2

---
+++ aoc2015/day2/part2.go
@@ -0,0 +1,54 @@
+package day2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Part2() {
+	file, err := os.Open("day2/input.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	result := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		arr := strings.Split(line, "x")
+
+		x, err := strconv.Atoi(arr[0])
+		if err != nil {
+			panic(err)
+		}
+
+		y, err := strconv.Atoi(arr[1])
+		if err != nil {
+			panic(err)
+		}
+
+		z, err := strconv.Atoi(arr[2])
+		if err != nil {
+			panic(err)
+		}
+
+		result += calcRibbonAndBow([]int{x, y, z})
+	}
+
+	fmt.Println(result)
+}
+
+func calcRibbonAndBow(arr []int) int {
+	sort.Ints(arr)
+	total := arr[0] * arr[1] * arr[2]
+
+	total += 2 * (arr[0] + arr[1])
+	return total
+}
--- aoc2015/main.go
+++ aoc2015/main.go
@@ -3,5 +3,5 @@ package main
 import "aoc2015/day2"
 
 func main() {
-	day2.Part1()
+	day2.Part2()
 }
